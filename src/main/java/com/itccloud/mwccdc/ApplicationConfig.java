626d66a63cca6ee45b0b65422b9a0e7eed8f9424
zongxi <zongxiliu@missouristate.edu>
2024-07-30 13:23:43 -0500

    thymeleaft basics

---
+++ src/main/java/com/itccloud/mwccdc/ApplicationConfig.java
@@ -0,0 +1,44 @@
+package com.itccloud.mwccdc;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class ApplicationConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new 
+        		ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean(name = "localeResolver")
+    public SessionLocaleResolver sessionLocaleResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("cn"));
+        return localeResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    } 
+}
--- src/main/java/com/itccloud/mwccdc/controller/HomeController.java
+++ src/main/java/com/itccloud/mwccdc/controller/HomeController.java
@@ -1,13 +1,40 @@
 package com.itccloud.mwccdc.controller;
 
 import org.springframework.stereotype.Controller;
+import java.util.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("message", "hello zongxi");
+		User user = new User("John Doe");
+		model.addAttribute("user", user);
 		return "index-form";
 	}
+	
+	@GetMapping("/advance")
+	public String advance(Model model) {
+		List<User> users1 = new ArrayList<User>();
+		users1.add(new User("Zongxi Liu"));
+		users1.add(new User("John Doe"));
+		users1.add(new User("Marry Ken"));
+		users1.add(new User("Mark Smith"));
+		
+		Map<String, User> users2 = new HashMap<String, User>();
+		users2.put("Zongxi Liu", new User("Zongxi Liu"));
+		users2.put("John Doe", new User("John Doe"));
+		
+		User[] users3 = new User[] {new User("Zongxi Liu"), new User("John Doe")};
+		
+		model.addAttribute("users1", users1);
+		model.addAttribute("users2", users2);
+		model.addAttribute("users3", users3);
+		
+		return "advance-form";
+	}
+	
 }
